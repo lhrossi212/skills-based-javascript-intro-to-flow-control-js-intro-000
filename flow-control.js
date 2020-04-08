c595c44461dfeef070ffe886544a0f578b8ee8ee
Lauren Rossi <lhrossi93@gmail.com>
2020-04-08 21:43:31 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -25,4 +25,12 @@ function ternaryTeenager(age) {
   return "You are not a teenager"
 }
 
-function switchAge(age)
+function switchAge(age) {
+  switch(age) {
+    case (age>=13 && age<=19):
+    console.log("You are a teenager");
+    break;
+    default:
+    console.log("You have an age");
+  }
+}
