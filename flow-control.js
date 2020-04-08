ec8bf0946f07e6ed87e72444fb44be0b6e208ac0
Lauren Rossi <lhrossi93@gmail.com>
2020-04-08 21:37:17 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -30,6 +30,6 @@ function switchAge(age) {
     case age>=13 && age<=19 :
     return "You are a teenager"
     break;
-    case age<13 && age>19
+    case age<13 || age>19
     return "You have an age" }
 }
