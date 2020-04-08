fc23cc9b2eacf2cb519d740004a459976d3ce086
Lauren Rossi <lhrossi93@gmail.com>
2020-04-08 21:51:30 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,5 @@
 function basicTeenager(age) {
-if (age>=13 && age<=19) {
+if (age >=13 && age <=19) {
 return "You are a teenager!" }
 }
 
@@ -7,13 +7,13 @@ return "You are a teenager!" }
 
 
 function teenager(age) {
-  if (age>=13 && age<=19) {
+  if (age >=13 && age <=19) {
   return "You are a teenager!"
 } else { return "You are not a teenager" }
 }
 
 function ageChecker(age) {
-  if (age>=13 && age<=19) {
+  if (age >=13 && age <=19) {
     return "You are a teenager!"
   } else if (age<=12) {
     return "You are a kid"
@@ -21,16 +21,27 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  return age>=13 && age<=19 ? return "You are a teenager" :
-  return "You are not a teenager"
+  return age >=13 && age <=19 ?
+  "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
-  switch(age) {
-    case (age>=13 && age<=19):
-    console.log("You are a teenager");
+  switch (age) {
+    case 13:
+    return "You are a teenager"
+    case 14:
+    return "You are a teenager"
+    case 15:
+    return "You are a teenager"
+    case 16:
+    return "You are a teenager"
+    case 17:
+    return "You are a teenager"
+    case 18:
+    return "You are a teenager"
+    case 19:
+    return "You are a teenager"
     break;
     default:
-    console.log("You have an age");
-  }
-}
+    return "You have an age"
+  } }
